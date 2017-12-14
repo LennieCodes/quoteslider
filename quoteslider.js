743cc2bd0e0a2c7f7df9c748fbb0be74b5c675f8
wocdomd <l.pinskiy@yahoo.com>
2017-12-13 21:51:21 -0500

    buggy, not quite there yet.

--- quoteslider.js
+++ quoteslider.js
@@ -6,29 +6,37 @@ const del = require('del');
 const interval = 60000;
 const path = '/Users/Lenny/Documents/applications/quoteslider/tmp/';
 let index = 0;
+let filename =  'wallpaper-' + index + '.jpg';
+let fullpath = path + filename;
+
 // wrap in setInterval Function
-setInterval(function() {
-    const filename = 'wallpaper-' + index + '.jpg';
-    const fullpath = path + filename;
+
+main();
+
+setInterval(main, interval);
+
+function main() {
     const imageNum = randomIntFromInterval(485, 5000000);
 
     del(path + '*.jpg').then(function() {
         
         request('https://quotefancy.com/download/' + imageNum + '/original/wallpaper.jpg')
         .pipe(fs.createWriteStream(fullpath))
-        .on('finish', setWallpaper);
-
-        index === 0 ? 1 : 0; // wallpaper won't change unless image name is different.
-
+        .on('finish', setWallpaper)
+        .then(function() {
+            filename = index === 0 ?  'wallpaper-' + 1 + '.jpg' :  'wallpaper-' + 0 + '.jpg'; // wallpaper won't change unless image name is different.
+            index = index === 0 ? 1 : 0;
+            fullpath = path + filename;
+        }); 
+       
     });
-
-}, interval)
-
+}
 
 function setWallpaper() {
-    wallpaper.set(fullpath).then(() => {
+    return wallpaper.set(fullpath).then(() => {
         console.log('wallpaper replaced');
-    })
+
+    });
 }
 
 // https://stackoverflow.com/questions/4959975/generate-random-number-between-two-numbers-in-javascript
